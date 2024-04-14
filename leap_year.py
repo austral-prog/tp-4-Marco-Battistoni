bec9eca682c24cfc373ff74746602418412f040b
Marco-Battistoni <162721472+Marco-Battistoni@users.noreply.github.com>
2024-04-14 01:02:09 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,15 @@
 def leap_year():
-    print("TO DO")
+    age = int(input("Ingrese un año: "))
+    age_m = age % 4
+    age_c = age % 100
+    age_q = age % 400
+
+    if age_m == 0 or age_c ==0:
+            if age_m == 0 and age_c !=0:
+                print(f"El año {age} es bisiesto")
+            elif age_c == 0 and age_q == 0:
+                print(f"El año {age} es bisiesto")
+            elif age_c == 0 and age_q != 0:
+                print(f"El año {age} no es bisiesto")
+    else:
+        print(f"El año {age} no es bisiesto")
